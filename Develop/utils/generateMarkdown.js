a33ea7e2a58fd30774c51dd5bad648662cc6d776
Lilyfields <lilyfields11@bigpond.com>
2022-02-07 20:01:37 +1100

    final commit

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,52 +1,66 @@
-class MarkDown {
+const inquirer = require('inquirer');
+const fs = require('fs');
+const index = require('../index');
+
+
   
-  static renderLicenseBadge(license) {
-    const badge = {
-      MIT: '[![Liscense: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
-      ISC:'[![Liscense:ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
-      GnuPG: '[![License:LGPL v3](https//img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)'
-          
+  function renderLicenseBadge(license) {
+    let badge = '';
+    if(license === 'MIT') {
+      badge = '![Liscense: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    }else if (license === 'Apache 2.0') {
+      badge = '![Liscense:ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
+    }else if (license === 'GPL v3.0') {
+      badge= '![License:LGPL v3](https//img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)'
+    }else {
+     badge = ''
     }
-    return badge[license]
-
-  }
-
-  static renderLicenseLink(license){
-    const licenseLink = {
-      MIT:'[MIT](https://choosealicense.com/licenses/mit/)',
-      ISC: '[ISC](https://choosealicense.com/licenses/isc/)',
-      GnuPG: '[GNUGPLv3](https://choosealicense.com/licenses/gpl-3.0/)',
+      return badge;
     }
 
-    return licenseLink[license]
+
+   function renderLicenseLink(license) {
+     let licenseLink = '';
+     if(license === 'MIT') {
+      licenseLink = 'https://opensource.org/licenses/MIT'
+    }else if (license === 'Apache 2.0') {
+      licenseLink = 'https://opensource.org/licenses/ISC'
+    }else if (license === 'GPL v3.0') {
+      licenseLink= 'https://www.gnu.org/licenses/lgpl-3.0'
+    }else {
+     licenseLink = '' 
+    }
+    return licenseLink;
   }
 
-  static renderLicenseSection(license){
-    if(license){
-      return `Licensed under the ${this.renderLicenseLink(license)} license`
-    }else{
-      return ''
+  function renderLicenseSection(license) {
+    let licenseSection = '';
+    if(license === 'None') {
+      licenseSection = ''
+    }else {
+      licenseSection=`License: ${license}`
     }
-  }
+      return licenseSection
+    }
   
-  static generateReadme(response){
+  
+  function generateMarkdown(response){
     return `
 
   # ${response.title}
-  ${this.renderLicenseBadge(response.license)}
 
-
+  
   ## Table of Content
-  -[Author](#author) 
-  -[Credits](#credits)
-  -[Motivation](#motivation)
-  -[Purpose](#purpose)
-  -[Key features](#key features)
-  -[Stage](#stage)
-  -[Future plan] (#future plan)
-  -[Email address] (#email address)
-  -[Github username] (#github username)
-  -[License] (#license)
+  ### * [Author](#author) 
+  ### * [Credits](#credits)
+  ### * [Motivation](#motivation)
+  ### * [Purpose](#purpose)
+  ### * [Key features](#keyFeatures)
+  ### * [Stage](#stage)
+  ### * [Future plan](#futurePlan)
+  ### * [Email address](#emailAddress)
+  ### * [Github username](#githubUsername)
+  ### * [License](#license)
 
   ## Author
   ${response.author}
@@ -76,11 +90,13 @@ class MarkDown {
   ${response. githubUsername}
 
   ## License
-  ${this.renderLicenseSection(response.license)}
+  ${renderLicenseBadge(response.license)} 
+  ${renderLicenseSection(response.license)}
+  ${renderLicenseLink(response.license)}
 
   `
   }
 
-}
+
   
-module.exports= MarkDown
+module.exports= generateMarkdown;
